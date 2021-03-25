c1dd9fa4c68b887c7823599d45f088c4b59f5d8e
Dsdroid1 <dhruvsharma2031@gmail.com>
2021-03-25 08:38:36 +0530

    Basic structure for app added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import CoinData from './components/coin-data';
+import CoinList from './components/coin-list';
+import CoinSearch from './components/coin-search';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <CoinData name='Bitcoin' id='1' price='1000' />
+
+        <CoinList/>
+
+        <CoinSearch/>
+
     </div>
   );
 }
---
+++ src/components/coin-list.jsx
@@ -0,0 +1,29 @@
+import React, { useState,useEffect } from 'react';
+import CoinData from './coin-data';
+
+const apiURL="https://api.nomics.com/v1/currencies/ticker?key=7986da46df6f3c84a80abcb10f1f7c73&ids=BTC,ETH,XRP&interval=1d,30d&convert=EUR&per-page=100&page=1";
+
+const CoinList = () => {
+    const [jsonData,setjsonData] = useState({coins:[]});
+    
+    useEffect(()=>{
+        APIcall();
+    },[]);
+
+    const APIcall = async () => {
+        const response = await fetch(apiURL);
+        const jsonFormat = await response.json();
+        setjsonData({coins:jsonFormat});
+    };
+
+
+    return ( 
+        <React.Fragment>
+            {jsonData.coins.map(item=>{
+                return <CoinData name={item.name} id={item.id} price={item.price}/>
+            } )}
+        </React.Fragment>
+    );
+}
+ 
+export default CoinList;
