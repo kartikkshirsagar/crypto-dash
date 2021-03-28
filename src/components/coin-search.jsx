fa0a14b8c6979827013e3b09b3dc9d1097db200d
Dsdroid1 <dhruvsharma2031@gmail.com>
2021-03-28 18:52:09 +0530

    Search works

--- src/components/coin-search.jsx
+++ src/components/coin-search.jsx
@@ -7,64 +7,35 @@ var query_part1 ="https://api.nomics.com/v1/currencies/ticker?key=7986da46df6f3c
 var query_part2="&interval=1d&convert=EUR";
 
 const CoinSearch = () => {
-    const [jsonData,setjsonData] = useState({coins:[]});
-    const [query,setQuery] =useState('bitcoin');
-    
-    const getCoinData= (query_string)=>{
-        var ids='';
-        var srch_string = '.*'+query_string+'.*';
-        console.log(srch_string)
-        for (var key of Object.keys(coinNames)){
-            if(key.includes(query_string) ){
-                ids=ids+coinNames[key]+',';
-                console.log('Matched:'+key)
-            }
-        }
-        if (ids!=''){
-            ids = ids.substr(0,ids.length-1);
-            console.log('idlist:'+ids);
-            var final_query=query_part1+ids+query_part2;
-            return final_query;
-        }
-        else{
-            return null;
-        }
-        
-    };
-    
-    useEffect(()=>{
+    const [jsonData,setjsonData] = useState(null);
+    const [query,setQuery] =useState('BTC');
+
+    //useEffect(()=>{
         //APIcall();
         //var Q=getCoinData(query);
         //if(Q!=null){
         //    QueryAPIcall(Q);
         //}
-    },[query]);
+    //},[query]);
 
-    const APIcall = async () => {
-        const response = await fetch(apiURL);
-        const jsonFormat = await response.json();
-        setjsonData({coins:jsonFormat});
-    };
-
+    
 
     const QueryAPIcall = async (url) => {
         const response = await fetch(url);
         
         const data = await response.json();
 
-        // check for error response
-        if (!response.ok) {
-            // get error message from body or default to response statusText
-            const error = (data && data.message) || response.statusText;
-            return Promise.reject(error);
-        }
+        if(data!=null && data[0]!=null){
         console.log(response);
         console.log(data);
-        setjsonData({coins:data});
+        setjsonData(data[0]);
+        console.log(data[0].id);
+        }
+
     };
 
     const runAPI= ()=>{
-        var url=query_part1+query+query_part2;
+        var url=query_part1+coinNames[query]+query_part2;
         QueryAPIcall(url);
     };
 
@@ -72,7 +43,7 @@ const CoinSearch = () => {
         <React.Fragment>
             <input type='text' value={query} onChange={event=>setQuery(event.target.value)}></input>
             <input type='button' onClick={()=>{runAPI()}}/>
-            {jsonData.coins.map((item,index)=>{
+            {/*{jsonData.coins.map((item,index)=>{
                 if(item.name===query){
                     console.log(item);
                     return <CoinData name={item.name} id={item.id} price={item.price} key={index}/>
@@ -80,7 +51,9 @@ const CoinSearch = () => {
                 else{
                     return null;
                 }
-            } )}
+            } )}*/}
+            { jsonData!=null &&
+            <CoinData key={jsonData.id} item={jsonData}/>}
         </React.Fragment>
     );
 }
