edf9b6c5d548e99a8ea46764895aedf5c3bb1214
Kartik Kshirsagar <kartikpkshirsagar@gmail.com>
2021-03-29 14:59:28 +0530

    moved row header to another component

--- src/App.js
+++ src/App.js
@@ -7,68 +7,18 @@ import Graph from "./components/graph";
 import Appbar from "./components/app-bar";
 import UseAutocomplete from "./components/autocomplete";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import RowHeader from "./components/row_header";
 
 
 
-const StyledTableCell = withStyles((theme) => ({
-  head: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  body: {
-    fontSize: 14,
-  },
-}))(TableCell);
-
-
-const useStyles = makeStyles({
-  table: {
-    minWidth: 700,
-  },
-});
-
-
-const StyledTableRow = withStyles((theme) => ({
-  root: {
-    '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.action.hover,
-    },
-  },
-}))(TableRow);
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
 
 function App(props) {
   return (
     <div className="App">
       <Appbar />
-      
       <UseAutocomplete />
-      <div className="container mt-5"></div>
-      <TableContainer component={Paper}>
-      <Table  aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell width="25%">Name</StyledTableCell>
-            <StyledTableCell align="left" width="25%">LOGO</StyledTableCell>
-            <StyledTableCell align="center" width="25%">Price</StyledTableCell>
-            <StyledTableCell align="center" width="25%">24H change</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        </Table>
-        </TableContainer>
+      <RowHeader />
       <CoinList />
-      {/*<CoinSearch/> */}
-      
     </div>
   );
 }
--- src/components/coin-list.jsx
+++ src/components/coin-list.jsx
@@ -1,33 +1,30 @@
-import React, { useState,useEffect } from 'react';
-import CoinData from './coin-data';
+import React, { useState, useEffect } from "react";
+import CoinData from "./coin-data";
 
-const apiURL="https://api.nomics.com/v1/currencies/ticker?key=7986da46df6f3c84a80abcb10f1f7c73&interval=1d&convert=INR&per-page=10&page=1";
-
-
-
-
+const apiURL =
+  "https://api.nomics.com/v1/currencies/ticker?key=7986da46df6f3c84a80abcb10f1f7c73&interval=1d&convert=INR&sort=rank&per-page=20&page=1";
 
 const CoinList = () => {
-    const [jsonData,setjsonData] = useState({coins:[]});
-    
-    useEffect(()=>{
-        APIcall();
+  const [jsonData, setjsonData] = useState({ coins: [] });
 
-    },[]);
+  useEffect(() => {
+    APIcall();
+  }, []);
 
-    const APIcall = async () => {
-        const response = await fetch(apiURL);
-        const jsonFormat = await response.json();
-        setjsonData({coins:jsonFormat});
-    };
+  const APIcall = async () => {
+    const response = await fetch(apiURL );
+    const jsonFormat = await response.json();
+    setjsonData({ coins: jsonFormat });
+  };
 
-    return ( 
-        <React.Fragment>
-            {jsonData.coins.map(item=>{
-                return <CoinData key={item.id} item={item}/>
-            } )}
-        </React.Fragment>
-    );
-}
- 
+  return (
+    <React.Fragment> 
+        {jsonData.coins.map((item) => {
+            
+          return <CoinData key={item.id} item={item} />;
+        })}
+    </React.Fragment>
+  );
+};
+
 export default CoinList;
